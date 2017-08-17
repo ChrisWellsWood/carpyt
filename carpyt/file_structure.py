a2a74c69359592049eee6d80c649720a91e4a873
Chris Wells Wood <cwwoodesq@gmail.com>
2017-08-17 23:57:40 +0100

    Adds basic skeleton for file structure function.

---
+++ carpyt/file_structure.py
@@ -0,0 +1,12 @@
+"""Creates the file structure for the project."""
+
+
+def make_file_structure(project_name: str):
+    """Creates the base file structure for the project.
+
+    Parameters
+    ----------
+    project_name : str
+        User supplied name for the project.
+    """
+    return

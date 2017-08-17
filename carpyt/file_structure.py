994633b15f6ff525e3153a1b3b8bdc6db3bd8fdd
Chris Wells Wood <cwwoodesq@gmail.com>
2017-08-18 00:33:17 +0100

    Adds project_dir variable for path.

--- carpyt/file_structure.py
+++ carpyt/file_structure.py
@@ -1,12 +1,16 @@
 """Creates the file structure for the project."""
 
+from pathlib import Path
 
-def make_file_structure(project_name: str):
+
+def make_file_structure(project_name: str, project_dir: Path):
     """Creates the base file structure for the project.
 
     Parameters
     ----------
     project_name : str
         User supplied name for the project.
+    project_dir : pathlib.Path
+        A path to the folder where the project will be created.
     """
     return

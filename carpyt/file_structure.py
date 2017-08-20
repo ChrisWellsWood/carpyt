19a05045acd0ffbea8f846b6759a6b913863e40b
Chris Wells Wood <cwwoodesq@gmail.com>
2017-08-20 16:02:51 +0100

    Adds make_file_structure and tests

--- carpyt/file_structure.py
+++ carpyt/file_structure.py
@@ -3,14 +3,26 @@
 from pathlib import Path
 
 
-def make_file_structure(project_name: str, project_dir: Path):
+def make_file_structure(
+        project_name: str, parent: Path, bin_project: bool=False):
     """Creates the base file structure for the project.
 
     Parameters
     ----------
     project_name : str
         User supplied name for the project.
-    project_dir : pathlib.Path
+    parent : pathlib.Path
         A path to the folder where the project will be created.
+    bin_project : bool
+        If True, a binary folder will be created.
     """
+    project = parent / project_name
+    module = project / project_name
+    docs = project / 'docs'
+    tests = project / 'tests'
+    for directory in [project, module, docs, tests]:
+        directory.mkdir()
+    if bin_project:
+        bin_d = project / 'bin'
+        bin_d.mkdir()
     return
--- tests/test_file_structure.py
+++ tests/test_file_structure.py
@@ -10,11 +10,31 @@ from carpyt.file_structure import make_file_structure
 class TestFileCreation(TestCase):
     """Tests that all required files are generated correctly."""
 
-    def test_module_creation(self):
+    def test_lib_creation(self):
         """Makes sure that the base directory structure is created."""
         with tempfile.TemporaryDirectory() as test_dir:
             test_dir_path = Path(test_dir)
             project_name = 'lovely_project'
             make_file_structure(project_name, test_dir_path)
-            project_dir = test_dir_path / project_name
-            self.assertTrue(project_dir.is_dir())
+            project = test_dir_path / project_name
+            module = project / project_name
+            docs = project / 'docs'
+            tests = project / 'tests'
+            for directory in [project, module, tests, docs]:
+                self.assertTrue(directory.exists())
+            bin_d = project / 'bin'
+            self.assertFalse(bin_d.exists())
+
+    def test_bin_creation(self):
+        """Makes sure that the base directory structure is created."""
+        with tempfile.TemporaryDirectory() as test_dir:
+            test_dir_path = Path(test_dir)
+            project_name = 'lovely_project'
+            make_file_structure(project_name, test_dir_path, bin_project=True)
+            project = test_dir_path / project_name
+            module = project / project_name
+            docs = project / 'docs'
+            tests = project / 'tests'
+            bin_d = project / 'bin'
+            for directory in [project, module, tests, docs, bin_d]:
+                self.assertTrue(directory.exists())

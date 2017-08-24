9e8eee3d8ca7dc6273d503c962c828f2aca6e76b
Chris Wells Wood <cwwoodesq@gmail.com>
2017-08-25 00:02:12 +0100

    Adds tests for new structure.

--- tests/test_file_structure.py
+++ tests/test_file_structure.py
@@ -4,7 +4,7 @@ import os
 from pathlib import Path
 from unittest import TestCase
 
-from carpyt import parse_template
+import carpyt
 
 
 TEST_TEMPLATES = Path(os.path.abspath(__file__)).parent / 'test_templates'
@@ -13,21 +13,15 @@ TEST_TEMPLATES = Path(os.path.abspath(__file__)).parent / 'test_templates'
 class TestTemplateParsing(TestCase):
     """Tests that templates are parsed correctly."""
 
-    def test_std_template(self):
-        """Tests the creation of a simple parse tree."""
-        template_path = TEST_TEMPLATES / 'simple.yml'
-        file_tree = parse_template(template_path)
-        for exp_file in ['setup.py', 'requirements.txt', '{module_name}/']:
-            self.assertTrue(exp_file in file_tree)
-        return
-
-    def test_nested_template(self):
+    def test_simple_template(self):
         """Tests creation of nested parse tree."""
-        template_path = TEST_TEMPLATES / 'nested_parent.yml'
-        file_tree = parse_template(template_path)
-        for exp_file in ['setup.py', 'requirements.txt', '{module_name}/']:
-            self.assertTrue(exp_file in file_tree)
-        return
+        template_path = TEST_TEMPLATES / 'nested_simple.yml'
+        file_tree = carpyt.run_template_parser(template_path)
+        self.assertTrue(file_tree.name == 'nested_simple')
+        self.assertTrue(file_tree[0].name == '{module}')
+        self.assertTrue(file_tree[0][0].name == 'tests')
+        self.assertTrue(file_tree[0][0][0].name == 'test.py')
+        self.assertTrue(file_tree[0][0][0].content is None)
 
 
 class TestFileCreation(TestCase):

24a71ca4513ab763519effe5f54fbf4d27e54b52
Chris Wells Wood <cwwoodesq@gmail.com>
2017-08-25 00:10:27 +0100

    Adds test for branched file structures.

--- tests/test_file_structure.py
+++ tests/test_file_structure.py
@@ -23,6 +23,19 @@ class TestTemplateParsing(TestCase):
         self.assertTrue(file_tree[0][0][0].name == 'test.py')
         self.assertTrue(file_tree[0][0][0].content is None)
 
+    def test_branched_template(self):
+        """Tests creation of branched parse tree."""
+        template_path = TEST_TEMPLATES / 'nested_branched.yml'
+        file_tree = carpyt.run_template_parser(template_path)
+        self.assertTrue(file_tree.name == 'nested_branched')
+        self.assertTrue(file_tree[0].name == '{module}')
+        self.assertTrue(file_tree[0][0].name == '__init__.py')
+        self.assertTrue('content' in file_tree[0][0].content)
+        self.assertTrue(file_tree[1].name == 'tests')
+        self.assertTrue(file_tree[1][0].name == 'test_files')
+        self.assertTrue(file_tree[1][0].content is None)
+        self.assertTrue(file_tree[2].name == 'docs')
+
 
 class TestFileCreation(TestCase):
     """Tests that all required files are generated correctly."""
